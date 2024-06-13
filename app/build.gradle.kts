0df2e0d5d956f3ab2d02af9b7f70ff5fea5ca58d
Kawtharmashkour <kawtharmashkour@yahoo.com>
2024-06-13 08:39:50 -0400

    change to app:Gravity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,5 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     kapt ("androidx.room:room-compiler:2.6.1")
-    // Material Design 3
-    implementation("com.google.android.material:material:1.12.0")
-
+    
 }
